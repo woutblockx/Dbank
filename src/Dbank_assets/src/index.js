693532ff5fc824d61fa927c8ccd1512833cbb044
Wout Blockx <woutblockx@Laptopke-van-Wout.local>
2022-10-11 16:34:05 +0200

    v1.0

--- src/Dbank_assets/src/index.js
+++ src/Dbank_assets/src/index.js
@@ -1,19 +1,34 @@
 import { Dbank } from "../../declarations/Dbank";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+// displays the current value
+window.addEventListener("load", async function() {
+  const currentAmount = await Dbank.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+});
 
-  const name = document.getElementById("name").value.toString();
+//top up and withdraw functionality
+document.querySelector("form").addEventListener("submit", async function(event) {
+  event.preventDefault();
+
+  const button = event.target.querySelector("#submit-btn");
+
+  const inputAmount = parseFloat(document.getElementById("input-amount").value);
+  const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
 
   button.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await Dbank.greet(name);
+  if (document.getElementById("input-amount").value.length != 0){
+    await Dbank.topUp(inputAmount);
+  }
 
+  if (document.getElementById("withdrawal-amount").value.length != 0) {
+    await Dbank.withdraw(outputAmount);
+  }
+
+  const currentAmount = await Dbank.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+
+  document.getElementById("input-amount").value = "";
+  document.getElementById("withdrawal-amount").value = "";
   button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
-});
+})
